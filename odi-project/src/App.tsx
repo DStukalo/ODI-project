5dab21b9cf0178f3a13cdcb2831d76b2142ad371
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-11-11 18:01:48 +0200

    refactor: delete app.css

--- odi-project/src/App.tsx
+++ odi-project/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
-
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<p>
-          Edit <code>src/App.tsx</code> and save to reload.
+					Edit
+					{' '}
+					<code>src/App.tsx</code>
+					{' '}
+					and save to reload.
 				</p>
 				<a
 					className="App-link"
@@ -14,7 +15,7 @@ function App() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-          Learn React
+					Learn React
 				</a>
 			</header>
 		</div>
