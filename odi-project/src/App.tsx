3ab7821ab39b2c828fe85d91dfa8ea78148b4a07
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-11-12 18:38:12 +0200

    feat: implement routing

--- odi-project/src/App.tsx
+++ odi-project/src/App.tsx
@@ -1,24 +1,25 @@
+import { Route, Routes } from 'react-router';
+
+import { NotFoundPage } from './pages/404/NotFoundPage';
+import { AuthorizationPage } from './pages/Authorization page/Authorization';
+import { BoardPage } from './pages/Board page/Board';
+import { WelcomePage } from './pages/Welcome page/WelcomePage';
+import { Layout } from './components/Layout/Layout';
+import { ProfilePage } from './pages/Profile page/ProfilePage';
+import { MainPage } from './pages/Main page/MainPage';
+
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit
-					{' '}
-					<code>src/App.tsx</code>
-					{' '}
-					and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<WelcomePage />} />
+				<Route path="main" element={<MainPage />} />
+				<Route path="main/board" element={<BoardPage />} />
+				<Route path="authorization" element={<AuthorizationPage />} />
+				<Route path="profile" element={<ProfilePage />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Route>
+		</Routes>
 	);
 }
 
