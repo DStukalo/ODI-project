9b8bbf96d80af63d19864171bbce8efea4c455bd
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-11-17 21:04:41 +0200

    feat: realized animated stickcy header

--- odi-project/src/components/Header/Header.tsx
+++ odi-project/src/components/Header/Header.tsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import styles from './Header.module.scss';
 import { Navigation } from './Navigation/Navigation';
 
 export function Header() {
 	const isLogged = localStorage.getItem('isLogged') || 'false';
 
+	const [sticky, setSticky] = useState('');
+
+	const isSticky = () => {
+		const scrollTop = window.scrollY;
+		const stickyClass = scrollTop >= 80 ? 'is-sticky' : '';
+		setSticky(stickyClass);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', isSticky);
+		return () => {
+			window.removeEventListener('scroll', isSticky);
+		};
+	}, []);
+
+	const classes = sticky ? 'is-sticky' : 'header';
+
 	return (
-		<header>
+		<header className={styles[`${classes}`]}>
 			<div className={styles.container}>
 				<Navigation logged={isLogged} />
 			</div>
