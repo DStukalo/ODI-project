34b246eda86c20cc1187f98920a50ba99591d675
Olga Malkovich <olga.malkovich@gmail.com>
2022-11-18 12:41:12 +0300

    fix: rewrite component Register

--- odi-project/src/components/Register/Register.tsx
+++ odi-project/src/components/Register/Register.tsx
@@ -1,47 +1,78 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export function Register() {
-	const [name, setName] = useState('');
-	const [pass, setPass] = useState('');
-	const [email, setEmail] = useState('');
+	const [user, setUser] = useState({
+		firstName: '',
+		lastName: '',
+		username: '',
+		password: ''
+	});
 
-	const handleSubmit = (e: { preventDefault: () => void }) => {
+	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const { name, value } = e.target;
+		setUser((user) => ({ ...user, [name]: value }));
+	}
+
+	function handleSubmit (e: { preventDefault: () => void }) {
 		e.preventDefault();
 	};
 
 	return (
-		<div className="auth-form" onSubmit={handleSubmit}>
+		<div className="form-auth">
 			<h2>Register</h2>
-			<form className="register-form">
-				{/* <label htmlFor="name">Full name</label> */}
-				<input
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					name="name"
-					id="name"
-					placeholder="Full name"
-				/>
-				{/* <label htmlFor="pass">Password</label> */}
-				<input
-					value={pass}
-					onChange={(e) => setPass(e.target.value)}
-					type="password"
-					id="pass"
-					placeholder="Password"
-					name="paassword"
-				/>
-				{/* <label htmlFor="email">Email</label> */}
-				<input
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					type="email"
-					id="email"
-					placeholder="E-mail"
-					name="email"
-				/>
-				<button type="submit">Log In</button>
+			<form className="register-form" onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label htmlFor="firstname">First name</label>
+					<input
+						type="text"
+						name="firstName"
+						value={user.firstName}
+						onChange={handleChange}
+						id="name"
+						placeholder="First name"
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="lastname">Last name</label>
+					<input
+						type="text"
+						name="lastname"
+						value={user.lastName}
+						onChange={handleChange}
+						id="lastname"
+						placeholder="Last name"
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="username">Username</label>
+					<input
+						type="text"
+						name="username"
+						value={user.username}
+						onChange={handleChange}
+						id="username"
+						placeholder="Username"
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						name="password"
+						value={user.password}
+						onChange={handleChange}
+						id="password"
+						placeholder="Password"
+					/>
+				</div>
+				<div className="form-button">
+					<button type="submit">Register</button>
+					<Link to="/login" className="button button-link">
+						Already have an account? Login here.
+					</Link>
+				</div>
 			</form>
-			<button className="link-button" type="submit">Already have an account? Login here.</button>
 		</div>
 	);
 }
