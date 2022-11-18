a1c4af8561a019154bbc14b2211ac1aa46cb0e16
Olga Malkovich <olga.malkovich@gmail.com>
2022-11-18 19:19:40 +0300

    fix: add switching between components

--- odi-project/src/App.tsx
+++ odi-project/src/App.tsx
@@ -7,6 +7,8 @@ import { BoardPage } from './pages/Board page/Board';
 import { AuthorizationPage } from './pages/Authorization page/Authorization';
 import { ProfilePage } from './pages/Profile page/ProfilePage';
 import { NotFoundPage } from './pages/404/NotFoundPage';
+import { Register } from './components/Register/Register';
+import { Login } from './components/Login/Login';
 
 function App() {
 	return (
@@ -15,7 +17,10 @@ function App() {
 				<Route index element={<WelcomePage />} />
 				<Route path="main" element={<MainPage />} />
 				<Route path="main/board" element={<BoardPage />} />
-				<Route path="authorization" element={<AuthorizationPage />} />
+				<Route path="authorization" element={<AuthorizationPage />}>
+					<Route path="login" element={<Login />} />
+					<Route path="register" element={<Register />} />
+				</Route>
 				<Route path="profile" element={<ProfilePage />} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Route>
