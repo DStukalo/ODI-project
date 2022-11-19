886b7ef5c523bc4467138cc81520ac2b027093ec
Igor-Novitski <pro100nke@gmail.com>
2022-11-19 11:11:51 +0300

    feat: add welcome section layout

--- odi-project/src/pages/Welcome page/WelcomePage.tsx
+++ odi-project/src/pages/Welcome page/WelcomePage.tsx
@@ -1,5 +1,36 @@
+import { Button } from '@/components/Button/Button';
+import styles from './WelcomePage.module.scss';
+
+function switchPage() {
+	if (localStorage.getItem('isLogged') === 'false') {
+		window.location.href = 'main';// будет использоваться redux для смени состояния
+	} else {
+		window.location.href = 'authorization';// будет использоваться redux для смени состояния
+	}
+
+}
+
 export function WelcomePage() {
 	return (
-		<div>Welcome page</div>
+		<section className={styles.wrapper}>
+			<div className={styles.welcomeSection}>
+				<div className={styles.descriptionBlock}>
+					<h1 className={styles.mainTittle}>ODI project - best task management for teams</h1>
+					<p className={styles.description}>
+						This is a visual tool that gives a perfect overview of the current
+						work status and simplifies team collaboration and communication.
+						Let clients and team members collaborate easily in real-time by
+						sharing tasks, information, and comments, anytime and from anywhere
+					</p>
+					<Button
+						text="Get started"
+						classes="welcome__btn"
+						image="/images/icon-start.png"
+						callback={switchPage}
+					/>
+				</div>
+				<div className={styles.welcomeImg} />
+			</div>
+		</section>
 	);
 }
