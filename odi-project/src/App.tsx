d2316102e21eddba8a5a82dee109801244a5bfbb
Olga Malkovich <olga.malkovich@gmail.com>
2022-11-24 15:53:21 +0300

    feat: add authorization in the App

--- odi-project/src/App.tsx
+++ odi-project/src/App.tsx
@@ -5,16 +5,33 @@ import { Layout } from './components/Layout/Layout';
 import { WelcomePage } from './pages/Welcome page/WelcomePage';
 import { MainPage } from './pages/Main page/MainPage';
 import { BoardPage } from './pages/Board page/Board';
-import { AuthorizationPage } from './pages/Authorization page/Authorization';
+import { AuthorizationPage } from './pages/Authorization page/AuthorizationPage';
 import { ProfilePage } from './pages/Profile page/ProfilePage';
 import { NotFoundPage } from './pages/404/NotFoundPage';
 import { Register } from './components/Register/Register';
 import { Login } from './components/Login/Login';
 import { initialState } from './locales/initialState';
+// import { Context } from './locales/auth.context';
+// import { AuthToAPI } from './API/Authorization';
 
 export const TranslationContext = createContext(initialState);
 
 function App() {
+	// const [isAuthorize, setIsAuthorize] = useState(AuthToAPI.isAuthorize());
+
+	// useEffect(() => {
+	// 	if (isAuthorize) {
+	// 		const interval = setInterval(() => {
+	// 			if (!AuthToAPI.isAuthorize()) {
+	// 				setIsAuthorize(false);
+	// 				navigate('/');
+	// 				clearInterval(interval);
+	// 			}
+	// 		}, 1000 * 2 * 60);
+	// 		return () => clearInterval(interval);
+	// 	}
+	// }, [isAuthorize, navigate]);
+
 	return (
 		<TranslationContext.Provider value={initialState}>
 			<Routes>
