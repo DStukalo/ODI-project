1580c2b3bdafec464b3b47ddaf1afc04edbc4830
Olga Malkovich <olga.malkovich@gmail.com>
2022-11-24 15:54:39 +0300

    feat: add methods in the Authorization

--- odi-project/src/API/Authorization.ts
+++ odi-project/src/API/Authorization.ts
@@ -4,26 +4,53 @@ import { AuthenticationData, UserDataWithStatus } from '@/types/interfaces';
 import { BASE_URL } from './consts';
 
 export class AuthToAPI {
-	public path: string;
+	static path: string;
 
 	constructor(path = '/') {
-		this.path = path;
+		AuthToAPI.path = path;
 	}
 
-	async signup(name: string, login: string, pass: string): Promise<UserDataWithStatus> {
+	static async signup(
+		firstName: string,
+		lastName: string,
+		username: string,
+		password: string,
+	): Promise<UserDataWithStatus> {
 		const res = await axios.post(`${BASE_URL}${this.path}signup`, {
-			name: `${name}`,
-			login: `${login}`,
-			password: `${pass}`,
+			firstname: `${firstName}`,
+			lastname: `${lastName}`,
+			username: `${username}`,
+			password: `${password}`,
 		});
 		return { data: res.data, status: res.status };
 	}
 
-	async signin(login: string, pass: string): Promise<AuthenticationData> {
+	static async signin(login: string, pass: string): Promise<AuthenticationData> {
 		const res = await axios.post(`${BASE_URL}${this.path}signin`, {
 			login: `${login}`,
 			password: `${pass}`,
 		});
 		return { data: res.data, status: res.status };
+	}
+
+	static signout() {
+		localStorage.removeItem('username');
+		document.cookie = 'token=;max-age=-1';
+	}
+
+	static getCookie(name: string) {
+		const matches = document.cookie.match(
+			// eslint-disable-next-line no-useless-escape
+			new RegExp(`(?:^|; )${name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '$1')}=([^;]*)`),
+		);
+		return matches ? decodeURIComponent(matches[1]) : undefined;
+	}
+
+	static getToken() {
+		this.getCookie('token');
+	}
+
+	static isAuthorize() {
+		Boolean(this.getToken());
 	}
 }
