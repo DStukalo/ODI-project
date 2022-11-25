408398a999d3cbed32d64b9bece44d2d172a8619
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-11-25 20:37:33 +0200

    refactor: add asyncthunk to userSlice

--- odi-project/src/store/reducers/UserSlice.ts
+++ odi-project/src/store/reducers/UserSlice.ts
@@ -1,15 +1,44 @@
 import { UserData } from '@/types/interfaces';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { isValidToken } from '@/functions/isValidToken';
+import { AuthToAPI } from '@/API/Authorization';
 
 interface UserState {
   user: Omit<UserData, 'name'>;
 	isLogged: boolean;
+	expirationDate: Date | null;
+	isLoading: boolean;
 }
+
+interface UserParamToSignin {
+	login: string;
+	pass: string;
+}
+
+export const authorizeUser = createAsyncThunk(
+	'user/authorizeUser',
+	async ({ login, pass }: UserParamToSignin, { rejectWithValue }) => {
+		const authUser = new AuthToAPI('auth/');
+		const signinUser = await authUser.signin(login, pass);
+		if ((signinUser).status !== 200) {
+			rejectWithValue('Server Error');
+		}
+		const validToken = isValidToken(signinUser.data.token);
+		if (!validToken) {
+			return rejectWithValue('Server Error');
+		}
+		return {
+			login: validToken.login,
+			id: validToken.id,
+			expirationDate: validToken.expirationDate,
+		};
+	},
+);
 
 function getInitialState(): UserState {
 	if (localStorage.getItem('token')) {
-		const validToken = isValidToken();
+		const token = localStorage.getItem('token') as string;
+		const validToken = isValidToken(token);
 		if (validToken.isValid) {
 			return {
 				user: {
@@ -17,6 +46,8 @@ function getInitialState(): UserState {
 					login: validToken.login as string,
 				},
 				isLogged: validToken.isValid,
+				expirationDate: validToken.expirationDate as Date,
+				isLoading: false,
 			};
 		}
 	}
@@ -26,6 +57,8 @@ function getInitialState(): UserState {
 			login: 'empty',
 		},
 		isLogged: false,
+		expirationDate: null,
+		isLoading: false,
 	};
 }
 
@@ -35,13 +68,31 @@ export const userSlice = createSlice({
 	name: 'user',
 	initialState,
 	reducers: {
-		changeLogged(state, action: PayloadAction<boolean>) {
-			// eslint-disable-next-line no-param-reassign
+		unLogged(state, action) {
 			state.isLogged = action.payload;
+			state.user._id = 'empty';
+			state.user.login = 'empty';
+			state.isLoading = false;
 		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(authorizeUser.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(authorizeUser.fulfilled, (state, action) => {
+				state.user._id = action.payload.id as string;
+				state.user.login = action.payload.login as string;
+				state.expirationDate = action.payload.expirationDate as Date;
+				state.isLoading = false;
+			})
+			.addCase(authorizeUser.rejected, (state) => {
+				state.isLoading = false;
+				
+			})
 	},
 });
 
-export const { changeLogged } = userSlice.actions;
+export const { unLogged } = userSlice.actions;
 
 export default userSlice.reducer;
