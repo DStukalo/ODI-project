c4a18693171bb5a25ab846347fd59a968a502095
Olga Malkovich <olga.malkovich@gmail.com>
2022-11-26 21:54:11 +0300

    fix: update path for query

--- odi-project/src/API/Authorization.ts
+++ odi-project/src/API/Authorization.ts
@@ -10,17 +10,17 @@ export class AuthToAPI {
 		this.path = path;
 	}
 
-	async signup(name: string, login: string, password: string): Promise<UserDataWithStatus> {
-		const res = await axios.post(`${BASE_URL}${this.path}signup`, {
+	async signup(name: string, login: string, pass: string): Promise<UserDataWithStatus> {
+		const res = await axios.post(`${BASE_URL}auth${this.path}signup`, {
 			name: `${name}`,
 			login: `${login}`,
-			password: `${password}`,
+			password: `${pass}`,
 		});
 		return { data: res.data, status: res.status };
 	}
 
 	async signin(login: string, pass: string): Promise<AuthenticationData> {
-		const res = await axios.post(`${BASE_URL}${this.path}signin`, {
+		const res = await axios.post(`${BASE_URL}auth${this.path}signin`, {
 			login: `${login}`,
 			password: `${pass}`,
 		});
