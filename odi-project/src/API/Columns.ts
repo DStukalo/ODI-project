d028978ae6263bedccb9a07b7fc9aa2e29469ead
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-11-29 11:32:23 +0200

    refactor: update url in every methods of class, add create instance of class

--- odi-project/src/API/Columns.ts
+++ odi-project/src/API/Columns.ts
@@ -2,37 +2,36 @@ import { ColumnData, ColumnListData } from '@/types/interfaces';
 import axios from 'axios';
 import { BASE_URL } from './consts';
 
-export class ColumnsToAPI {
-	public path: string;
+class ColumnsToAPI {
+	private token: string;
 
-	constructor(path = '/') {
-		this.path = path;
+	constructor() {
+		this.token = localStorage.getItem('token') as string;
 	}
 
-	async getColumnsInBoardID(token: string, boardID: string): Promise< ColumnListData > {
+	async getAllColumnsInBoardID(boardID: string): Promise< ColumnListData > {
 		const res = await axios({
 			method: 'get',
-			url: `${BASE_URL}boards/${boardID}/${this.path}`,
+			url: `${BASE_URL}boards/${boardID}/columns`,
 			headers: {
 				Accept: 'application/json',
-				Authorization: `Bearer ${token}`,
+				Authorization: `Bearer ${this.token}`,
 			},
 		});
-		return { data: res.data, status: res.status };
+		return { data: res.data, status: res.status } as ColumnListData;
 	}
 
 	async createColumnInBoardID(
-		token: string,
 		title: string,
 		order: number,
 		boardID: string,
 	): Promise< ColumnData > {
 		const res = await axios({
 			method: 'post',
-			url: `${BASE_URL}boards/${boardID}/${this.path}`,
+			url: `${BASE_URL}boards/${boardID}/columns`,
 			headers: {
 				Accept: 'application/json',
-				Authorization: `Bearer ${token}`,
+				Authorization: `Bearer ${this.token}`,
 			},
 			data: {
 				title: `${title}`,
@@ -42,24 +41,19 @@ export class ColumnsToAPI {
 		return { data: res.data, status: res.status };
 	}
 
-	async getColumnsByIDInBoardID(
-		token: string,
-		boardID: string,
-		columnsID: string,
-	): Promise< ColumnData > {
+	async getColumnsByIDInBoardID(boardID: string, columnsID: string): Promise< ColumnData > {
 		const res = await axios({
 			method: 'get',
-			url: `${BASE_URL}boards/${boardID}/${this.path}/${columnsID}`,
+			url: `${BASE_URL}boards/${boardID}/columns/${columnsID}`,
 			headers: {
 				Accept: 'application/json',
-				Authorization: `Bearer ${token}`,
+				Authorization: `Bearer ${this.token}`,
 			},
 		});
 		return { data: res.data, status: res.status };
 	}
 
 	async updateColumnsByIDInBoardID(
-		token: string,
 		title: string,
 		order: number,
 		boardID: string,
@@ -67,11 +61,11 @@ export class ColumnsToAPI {
 	): Promise< ColumnData > {
 		const res = await axios({
 			method: 'put',
-			url: `${BASE_URL}boards/${boardID}/${this.path}/${columnsID}`,
+			url: `${BASE_URL}boards/${boardID}/columns/${columnsID}`,
 			headers: {
 				Accept: 'application/json',
 				'Content-Type': 'application/json',
-				Authorization: `Bearer ${token}`,
+				Authorization: `Bearer ${this.token}`,
 			},
 			data: {
 				title: `${title}`,
@@ -81,21 +75,19 @@ export class ColumnsToAPI {
 		return { data: res.data, status: res.status };
 	}
 
-	async deleteColumnsByIDInBoardID(
-		token: string,
-		boardID: string,
-		columnsID: string,
-	): Promise< number > {
+	async deleteColumnsByIDInBoardID(boardID: string, columnsID: string): Promise< number > {
 		const res = await axios({
 			method: 'delete',
-			url: `${BASE_URL}boards/${boardID}/${this.path}/${columnsID}`,
+			url: `${BASE_URL}boards/${boardID}/columns/${columnsID}`,
 			headers: {
 				Accept: 'application/json',
-				Authorization: `Bearer ${token}`,
+				Authorization: `Bearer ${this.token}`,
 			},
 		});
 		return res.status;
 	}
-
-	// Доделать 3 метода columnsSet
 }
+
+const columnsToAPI = new ColumnsToAPI();
+
+export default columnsToAPI;
