461785a23a266f35b325452e0a5460ea24e62990
Olga Malkovich <olga.malkovich@gmail.com>
2022-12-03 14:47:44 +0300

    feat: update Profile page

--- odi-project/src/pages/Profile page/ProfilePage.tsx
+++ odi-project/src/pages/Profile page/ProfilePage.tsx
@@ -2,104 +2,91 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/Button/Button';
 import { Input } from '@/components/Input/Input';
-import { UserData } from '@/types/interfaces';
 import userToAPI from '@/API/User';
+import { useAppSelector } from '@/hooks/redux';
+import { useTranslation } from '@/locales/useTranslation';
+import { Modal } from '@/components/Modal/Modal';
 import styles from './ProfilePage.module.scss';
 
-const userDefault = {
-	_id: '',
-	name: '',
-	login: '',
-};
-
 export function ProfilePage() {
+	const { user } = useAppSelector((state) => state.userReducer);
 	const navigate = useNavigate();
-	const [user, setUser] = useState<UserData>(userDefault);
-	const [users, setUsers] = useState<UserData[]>([]);
+	const newLocal = useTranslation();
+	const [name, setName] = useState('');
+	const [login, setLogin] = useState('');
 	const [password, setPassword] = useState('');
-
-	const getUsers = async () => {
-		try {
-			const { data } = await userToAPI.getUsers();
-			setUsers(data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	const getUser = async (ID: string) => {
-		try {
-			const { data } = await userToAPI.getUserByID(ID);
-			// setUser(data);
-			console.log(data);
-		} catch (error) {
-			console.log('Error');
-		}
-		// try {
-		// 	const loginuser = (localStorage.getItem('login') as string);
-		// 	const profile = users.find((x) => x.login === loginuser);
-		// 	if (profile) {
-		// 		setUser(profile);
-		// 		console.log(profile);
-		// 	}
-		// } catch (error) {
-		// 	console.log(error);
-		// }
-	};
+	const [showSuccessModal, setShowSuccessModal] = useState(false);
 
 	const deleteUser = async () => {
-		try {
-			await userToAPI.deleteUserByID(user._id);
-			getUsers();
-			navigate('/');
-		} catch (error) {
-			console.log(error);
-		}
+		await userToAPI.deleteUserByID(user._id);
+		navigate('/');
 	};
 
 	useEffect(() => {
-		getUsers();
-	}, []);
-
-	useEffect(() => {
-		getUser(user._id);
-	});
+		async function getUserID(ID: string) {
+			const { data } = await userToAPI.getUserByID(ID);
+			const profileId = data._id;
+			if (profileId) {
+				setName(data.name);
+				setLogin(data.login);
+			}
+		}
+		getUserID(user._id);
+	}, [user._id]);
 
 	const handleChangeName = (e: React.FormEvent<HTMLInputElement>) => {
-		setUser({ ...user, name: e.currentTarget.value as string });
+		setName(e.currentTarget.value as string);
 	};
 
 	const handleChangeLogin = (e: React.FormEvent<HTMLInputElement>) => {
-		setUser({ ...user, login: e.currentTarget.value as string });
+		setLogin(e.currentTarget.value as string);
 	};
 
 	const handleChangePassword = (e: React.FormEvent<HTMLInputElement>) => {
 		setPassword(e.currentTarget.value as string);
 	};
 
-	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
+		if (name && login && password) {
+			setName(name);
+			setLogin(login);
+			setPassword(password);
+			await userToAPI.updateUserByID({
+				ID: user._id, name, login, pass: password,
+			});
+			setShowSuccessModal(true);
+			setTimeout(() => navigate('/'), 2000);
+		}
 	}
 
 	return (
 		<div className={styles.profile_container}>
-			<h2>Profile</h2>
+			<h2>{newLocal.profileHeader}</h2>
+			{showSuccessModal && (
+				<Modal
+					title={`${newLocal.profileModalHeader} ${login}`}
+					buttonText={newLocal.modalButton}
+					onClose={setShowSuccessModal}
+					classes="modal_auth"
+				>
+					<h3>{newLocal.profileModalEdit}</h3>
+				</Modal>
+			)}
 			<form onSubmit={handleSubmit}>
-				<h3>Edit your profile</h3>
+				<h3>{newLocal.profileHeaderEdit}</h3>
 				<Input
 					type="text"
 					name="name"
-					value={user.name}
+					value={name}
 					autocomplete="name"
-					// placeholder={user.name}
 					classes="profile_input"
 					onChange={handleChangeName}
 				/>
 				<Input
 					type="text"
 					name="login"
-					value={user.login}
-					// placeholder={user.login}
+					value={login}
 					classes="profile_input"
 					onChange={handleChangeLogin}
 				/>
@@ -107,14 +94,12 @@ export function ProfilePage() {
 					type="password"
 					name="password"
 					value={password}
-					// placeholder={password}
 					classes="profile_input"
 					onChange={handleChangePassword}
 				/>
 				<div className={styles.form_buttons}>
-					<button type="submit">Save</button>
-					{/* <Button classes="" text="Save" /> */}
-					<Button classes="" text="Delete" callback={deleteUser} />
+					<button type="submit">{newLocal.profileButtonSave}</button>
+					<Button classes="" text={newLocal.profileButtonDelete} callback={deleteUser} />
 				</div>
 			</form>
 		</div>
